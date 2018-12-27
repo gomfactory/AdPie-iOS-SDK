18de03d285da5aaf1c9d2bb45a4a129b2fdc1718
YongSun Kim <sunny@gomfactory.com>
2018-12-27 12:09:53 +0900

    Update SDK

--- AdPieSwiftSample/AdPieSDK.framework/Versions/A/Headers/AdPieSDK.h
+++ AdPieSwiftSample/AdPieSDK.framework/Versions/A/Headers/AdPieSDK.h
@@ -15,7 +15,7 @@
 #import <AdPieSDK/APNativeAdView.h>
 #import <AdPieSDK/APTargetingData.h>
 
-#define ADPIE_SDK_VERSION @"1.2.1"
+#define ADPIE_SDK_VERSION @"1.2.2"
 
 @interface AdPieSDK : NSObject
 
