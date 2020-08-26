76507b38593540b7a8225c4e25a9eee661f28024
YongSun Kim <sunny@gomfactory.com>
2020-08-26 11:51:54 +0900

    update SDK

--- AdPieSDK/AdPieSDK.framework/Versions/A/Headers/AdPieSDK.h
+++ AdPieSDK/AdPieSDK.framework/Versions/A/Headers/AdPieSDK.h
@@ -15,7 +15,7 @@
 #import <AdPieSDK/APNativeAdView.h>
 #import <AdPieSDK/APTargetingData.h>
 
-#define ADPIE_SDK_VERSION @"1.2.8"
+#define ADPIE_SDK_VERSION @"1.2.7"
 
 typedef void (^ResultBlock)(BOOL isInitialized);
 
