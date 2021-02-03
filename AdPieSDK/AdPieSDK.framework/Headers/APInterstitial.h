837787ae4071b39dbb07d878a263ea046d5a76d5
YongSun Kim <sunny@gomfactory.com>
2021-02-03 17:20:53 +0900

    update AdPie SDK 1.2.9

---
+++ AdPieSDK/AdPieSDK.framework/Headers/APInterstitial.h
@@ -0,0 +1,50 @@
+//
+//  APInterstitial.h
+//  AdPieSDK
+//
+//  Created by sunny on 2016. 3. 2..
+//  Copyright © 2016년 GomFactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol APInterstitialDelegate;
+
+@interface APInterstitial : NSObject
+
+@property(weak, nonatomic) id<APInterstitialDelegate> delegate;
+
+@property(copy, nonatomic) NSString *slotId;
+
+- (id)initWithSlotId:(NSString *)slotId;
+
+- (void)load;
+
+- (void)presentFromRootViewController:(UIViewController *)controller;
+
+- (BOOL)isReady;
+
+@end
+
+@protocol APInterstitialDelegate <NSObject>
+
+@required
+
+// 전면배너 성공
+- (void)interstitialDidLoadAd:(APInterstitial *)interstitial;
+// 전면배너 실패
+- (void)interstitialDidFailToLoadAd:(APInterstitial *)interstitial
+                          withError:(NSError *)error;
+
+@optional
+// 전면배너 표출 알림
+- (void)interstitialWillPresentScreen:(APInterstitial *)interstitial;
+// 전면배너 종료 예정 알림
+- (void)interstitialWillDismissScreen:(APInterstitial *)interstitial;
+// 전면배너 종료 완료 알림
+- (void)interstitialDidDismissScreen:(APInterstitial *)interstitial;
+// 전면배너 클릭 알림
+- (void)interstitialWillLeaveApplication:(APInterstitial *)interstitial;
+
+@end
---
+++ AdPieSDK/AdPieSDK.framework/Headers/APNativeAdView.h
@@ -0,0 +1,35 @@
+//
+//  APNativeAdView.h
+//  AdPieSDK
+//
+//  Created by sunny on 2016. 8. 31..
+//  Copyright © 2016년 GomFactory. All rights reserved.
+//
+
+#import "APMainAdView.h"
+#import "APNativeAdData.h"
+#import <UIKit/UIKit.h>
+
+@protocol APNativeAdViewDelegate;
+
+@interface APNativeAdView : UIView
+
+@property(weak, nonatomic) IBOutlet UIImageView *iconImageView;
+@property(weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property(weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property(weak, nonatomic) IBOutlet APMainAdView *mainAdView;
+@property(weak, nonatomic) IBOutlet UIButton *callToActionButton;
+
+@property(nonatomic, readonly) bool isValidLayout;
+
+@property(weak, nonatomic) id<APNativeAdViewDelegate> delegate;
+
+- (BOOL)fillAd:(APNativeAdData *)adData;
+
+@end
+
+@protocol APNativeAdViewDelegate <NSObject>
+
+@optional
+- (void)viewClicked;
+@end
