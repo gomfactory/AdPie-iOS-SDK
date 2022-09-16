db05dc940650b842510dcd0aad56ead882cff3d2
YongSun Kim <sunny@adxcorp.kr>
2022-09-16 16:39:54 +0900

    [수정] 애드파이 SDK 1.3.5 업데이트

--- AdPieSDK/AdPieSDK.framework/Headers/AdPieSDK.h
+++ AdPieSDK/AdPieSDK.framework/Headers/AdPieSDK.h
@@ -16,7 +16,7 @@
 #import <AdPieSDK/APRewardedAd.h>
 #import <AdPieSDK/APTargetingData.h>
 
-#define ADPIE_SDK_VERSION @"1.3.4.1"
+#define ADPIE_SDK_VERSION @"1.3.5"
 
 typedef void (^ResultBlock)(BOOL isInitialized);
 
