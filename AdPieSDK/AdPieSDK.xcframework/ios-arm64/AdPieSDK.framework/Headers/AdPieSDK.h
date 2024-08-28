0f1dc584a82c2170e11ebc52d30ba60e9103153e
jclee <jclee@adxcorp.kr>
2024-08-28 15:23:39 +0900

    [수정] AdPieSDK 업데이트 (1.6.3)

--- AdPieSDK/AdPieSDK.xcframework/ios-arm64/AdPieSDK.framework/Headers/AdPieSDK.h
+++ AdPieSDK/AdPieSDK.xcframework/ios-arm64/AdPieSDK.framework/Headers/AdPieSDK.h
@@ -16,7 +16,7 @@
 #import <AdPieSDK/APRewardedAd.h>
 #import <AdPieSDK/APTargetingData.h>
 
-#define ADPIE_SDK_VERSION @"1.6.2"
+#define ADPIE_SDK_VERSION @"1.6.3"
 
 typedef void (^ResultBlock)(BOOL isInitialized);
 
